ff5d66779c7fbe9e8f50200300d0a954ed099939
Marina Sokolova <umarishki@mail.ru>
2022-07-20 12:10:05 +0800

    Add console log logger

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,20 +1,20 @@
-import { Api } from '../utils/api';
+// import { Api } from '../utils/api';
 
-export const apiAuth = new Api({
-    baseUrl: 'http://localhost:3000/',
-    headers: {
-        "Content-Type": "application/json"
-    }
-});
+// export const apiAuth = new Api({
+//     baseUrl: 'http://localhost:3000/',
+//     headers: {
+//         "Content-Type": "application/json"
+//     }
+// });
 
-export const createApiAuthCheck = (token) => {
-    if (token) {
-        return new Api({
-            baseUrl: 'http://localhost:3000/',
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`
-            }
-        });
-    }
-}
+// export const createApiAuthCheck = (token) => {
+//     if (token) {
+//         return new Api({
+//             baseUrl: 'http://localhost:3000/',
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Authorization": `Bearer ${token}`
+//             }
+//         });
+//     }
+// }
